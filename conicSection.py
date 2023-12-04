e6fcef3c00c4eec3ffa5778dcb255e54d3e34d3f
anatla <aaa@labis.su>
2023-12-04 13:18:13 +0900

    .

--- conicSection.py
+++ conicSection.py
@@ -10,13 +10,25 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(859, 844)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.setGeometry(QtCore.QRect(30, 30, 100, 30))
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setGeometry(QtCore.QRect(145, 30, 100, 30))
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.setGeometry(QtCore.QRect(260, 30, 100, 30))
+
         MainWindow.setCentralWidget(self.centralwidget)
+
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -27,7 +39,10 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Кривые второго порядка"))
+
         self.pushButton.setText(_translate("MainWindow", "Эллипс"))
+        self.pushButton_2.setText(_translate("MainWindow", "Гипербола"))
+        self.pushButton_3.setText(_translate("MainWindow", "Парабола"))
 
 
 class ClssDialog(QtWidgets.QDialog):
@@ -36,12 +51,16 @@ class ClssDialog(QtWidgets.QDialog):
 
         self.verticalLayout = QtWidgets.QVBoxLayout(self)
         self.verticalLayout.setObjectName("verticalLayout")
+
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(70, 70, 19, 25))
+
         self.pushButton = QtWidgets.QPushButton(self)
         self.pushButton.setObjectName("pushButton")
         self.pushButton.clicked.connect(self.btnClosed)
         self.verticalLayout.addWidget(self.pushButton)
-        self.setWindowTitle("Dialog")
-        self.pushButton.setText("Close Dialog")
+        self.setWindowTitle("Кривая")
+        self.pushButton.setText("Закрыть")
 
     def btnClosed(self):
         self.close()
